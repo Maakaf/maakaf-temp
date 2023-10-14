4f43755673f8d78639523b41eb3936db72455d9f
Daniel <danirdd92@gmail.com>
2023-10-14 22:17:43 +0300

    initial setup for sidebar navigation

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,3 +1,21 @@
 import { defineCollection, z } from "astro:content";
 
-export const collections = {};
+// const blog = defineCollection({
+//   // Type-check frontmatter using a schema
+//   schema: z.object({
+//     title: z.string(),
+//     description: z.string(),
+//     // Transform string to Date object
+//     pubDate: z.coerce.date(),
+//     updatedDate: z.coerce.date().optional(),
+//     heroImage: z.string().optional(),
+//   }),
+// });
+
+const newbies = defineCollection({
+  schema: z.object({
+    title: z.string(),
+  }),
+});
+
+export const collections = { newbies };
