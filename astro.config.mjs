673f4b92b7e886ea87e7876f484294e2aab870fc
gal <gal.eindor@gmail.com>
2023-10-18 10:34:09 +0300

    add react intergation

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,6 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
+import react from '@astrojs/react';
 import sitemap from '@astrojs/sitemap';
 
 import tailwind from "@astrojs/tailwind";
@@ -7,7 +8,7 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://maakaf.netlify.app/',
-  integrations: [sitemap(), tailwind({
+  integrations: [sitemap(), react(), tailwind({
     applyBaseStyles: false
   })]
 });
