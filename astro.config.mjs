3a78db2648446bb9450edb79229c9d8389864e69
gal <gal.eindor@gmail.com>
2023-10-19 14:00:54 +0300

    rebase 5/8

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,18 +2,15 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import react from '@astrojs/react';
 import sitemap from '@astrojs/sitemap';
-
 import tailwind from "@astrojs/tailwind";
+
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://maakaf.netlify.app/',
-  integrations: [
-    sitemap(),
-    react({
-      include: ['**/react/*'],
-    }), 
-    tailwind({
+  integrations: [sitemap(), mdx(), react({
+    include: ['**/react/*']
+  }), tailwind({
     applyBaseStyles: false
   })]
 });
